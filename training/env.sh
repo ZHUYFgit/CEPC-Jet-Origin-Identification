18299f2dfe0cec4c005f276c1664eafbced6e6da
ZHUYFgit <33801128+ZHUYFgit@users.noreply.github.com>
2024-08-22 15:02:37 +0800

    Add files via upload

---
+++ training/env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source env_conda.sh
+
+export DATADIR_JetClass=/hpcfs/cepc/higgsgpu/zhuyf/particle_transformer/dataset/JetClass
+export DATADIR_TopLandscape=
+export DATADIR_QuarkGluon=
